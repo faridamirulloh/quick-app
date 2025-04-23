7ee092766013ac93b68c3abab8736c5a05f75644
faridamirulloh <faridamirulloh01@gmail.com>
2025-04-23 08:30:30 +0700

    test sidebar

--- src/contents/main/Sidebar.jsx
+++ src/contents/main/Sidebar.jsx
@@ -3,7 +3,11 @@ import React from 'react';
 import style from './Sidebar.module.scss';
 
 function Sidebar() {
-  return <div id="main-sidebar" className={style.container} />;
+  return (
+    <div id="main-sidebar" className={style.container}>
+      <span style={{ color: 'white' }}>Sidebar</span>
+    </div>
+  );
 }
 
 export default Sidebar;
