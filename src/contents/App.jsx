b8989beb9ecdd2cb9fa0a854e7b229f8239b04ba
faridamirulloh <faridamirulloh01@gmail.com>
2025-04-23 09:16:23 +0700

    test

--- src/contents/App.jsx
+++ src/contents/App.jsx
@@ -3,13 +3,13 @@ import React from 'react';
 import 'react-datepicker/dist/react-datepicker.css';
 import './App.scss';
 import Content from './main/Content';
-import Sidebar from './main/SideBar';
+// import Sidebar from './main/SideBar';
 import Quicks from './quicks/quicks';
 
 function App() {
   return (
     <div className="app">
-      <Sidebar />
+      {/* <Sidebar /> */}
       <Content />
       <Quicks />
     </div>
